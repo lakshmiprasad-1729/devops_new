a89772ff538722a939a7a7814830239a76c4bceb
lakshmiprasad-1729 <panchagirilakshminarayana@gmail.com>
2025-08-22 12:54:04 +0530

    added dockerfile

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.get('/',(req,res)=>{
     res.send("it is Paas "+new Date());
 })
 
-app.listen(4000,()=>{
-    console.log("app listening on port 4000");
+app.listen(9001,()=>{
+    console.log("app listening on port 9001");
 })
