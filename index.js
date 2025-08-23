f23110d7c362c0b06c621daccced69c8da6b14a3
lakshmiprasad-1729 <panchagirilakshminarayana@gmail.com>
2025-08-23 16:52:13 +0530

    added a  example sample

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.get('/',(req,res)=>{
     res.send("it is Paas "+new Date());
+    
+    // Change this line
+    res.send('Hello from my PaaS! Deployed on: ' + new Date().toLocaleString());
+    // To this
+    res.send('AUTO-DEPLOYMENT IS AWESOME! Deployed on: ' + new Date().toLocaleString());
 })
 
 app.listen(9001,()=>{
